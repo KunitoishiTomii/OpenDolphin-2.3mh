0c10178f031a9ba9cc480bc20b45b89b62476673
kim <kim@kim-VAIO>
2013-03-22 17:39:17 +0900

    httpsアクセスをしなければいいだけだったので本家との互換性を最大限保つように修正（実質ほぼRevert)

--- client/src/main/java/open/dolphin/delegater/RESTEasyClient.java
+++ client/src/main/java/open/dolphin/delegater/RESTEasyClient.java
@@ -1,9 +1,16 @@
 package open.dolphin.delegater;
 
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import javax.ws.rs.core.MultivaluedMap;
 import open.dolphin.client.Dolphin;
 import open.dolphin.infomodel.IInfoModel;
 import open.dolphin.util.HashUtil;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
@@ -22,6 +29,8 @@ public class RESTEasyClient {
     private String fidUid;
     private String passMD5;
     
+    private SSLSocketFactory sslSocketFactory;
+    
     private static final RESTEasyClient instance;
     
     static {
@@ -30,6 +39,10 @@ public class RESTEasyClient {
 
     private RESTEasyClient() {
         clientUUID = Dolphin.getInstance().getClientUUID();
+        try {
+            setupOreOreSSL();
+        } catch (Exception ex) {
+        }
     }
 
     public static RESTEasyClient getInstance() {
@@ -47,6 +60,8 @@ public class RESTEasyClient {
     }
 
     public void setBaseURI(String uri) {
+        // 手抜きｗ
+//        uri = uri.replace("http", "https").replace(":8080", ":8443");
         baseURI = uri;
     }
     
@@ -72,6 +87,8 @@ public class RESTEasyClient {
     public ClientRequest getClientRequest(String path) {
         
         DefaultHttpClient httpClient = new DefaultHttpClient();
+        secureHttpClient(httpClient);
+        
         HttpParams params = httpClient.getParams();
         HttpConnectionParams.setConnectionTimeout(params, 30 * 1000);
         HttpConnectionParams.setSoTimeout(params, 0);
@@ -84,11 +101,75 @@ public class RESTEasyClient {
         
         return request;
     }
- 
-    private String[] splitFidUid(String username) {
-        int pos = username.indexOf(IInfoModel.COMPOSITE_KEY_MAKER);
-        String fid = username.substring(0, pos);
-        String uid = username.substring(pos + 1);
-        return new String[]{fid, uid};
+    
+    // SSL証明書を受け入れる
+    private void secureHttpClient(HttpClient httpClient) {
+        
+        if (sslSocketFactory == null) {
+            return;
+        }
+        //register https protocol in httpclient's scheme registry
+        SchemeRegistry sr = httpClient.getConnectionManager().getSchemeRegistry();
+        Scheme https = new Scheme("https", 443, sslSocketFactory);    // 8443?
+        sr.register(https);    
     }
+    
+    private void setupOreOreSSL() throws Exception {
+
+        TrustStrategy ts = new TrustStrategy() {
+            @Override
+            public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                return true; // heck yea!
+            }
+        };
+        
+        // build socket factory with hostname verification turned off.
+        sslSocketFactory = new SSLSocketFactory(ts, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+    }
+    
+/*
+    private void setupOreOreSSL() throws NoSuchAlgorithmException, KeyManagementException {
+
+        //Secure Protocol implementation.
+        SSLContext ctx = SSLContext.getInstance("TLS");
+        
+        //Implementation of a trust manager for X509 certificates
+        X509TrustManager manager = new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+        };
+
+        X509HostnameVerifier verifier = new X509HostnameVerifier() {
+            @Override
+            public void verify(String string, SSLSocket ssls) throws IOException {
+            }
+
+            @Override
+            public void verify(String string, X509Certificate xc) throws SSLException {
+            }
+
+            @Override
+            public void verify(String string, String[] strings, String[] strings1) throws SSLException {
+            }
+
+            @Override
+            public boolean verify(String string, SSLSession ssls) {
+                return true;
+            }
+        };
+        
+        ctx.init(null, new TrustManager[]{manager}, null);
+        sslSocketFactory = new SSLSocketFactory(ctx, verifier);
+    }
+*/
 }
