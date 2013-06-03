96bd7b9c86ad96c690413080cbf0cae1c564b39a
masuda <masuda@masuda-PC>
2013-06-03 21:11:05 +0900

    ２号用紙形式KarteEditorのスクロールバーを左右共通に

--- client/src/main/java/open/dolphin/client/KartePanel2.java
+++ client/src/main/java/open/dolphin/client/KartePanel2.java
@@ -3,9 +3,12 @@ package open.dolphin.client;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.Scrollable;
+import javax.swing.SwingConstants;
 
 /**
  * ２号カルテパネル
@@ -26,22 +29,30 @@ public final class KartePanel2 extends KartePanel {
 
         soaTextPane = createTextPane();
         pTextPane = createTextPane();
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(rows, cols, hgap, vgap));
+
         
         if (editor) {
+            JPanel panel = new DoubleScrollPanel();
+            panel.setLayout(new GridLayout(rows, cols, hgap, vgap));
+            panel.add(soaTextPane);
+            panel.add(pTextPane);
+            JScrollPane scroll = new JScrollPane(panel);
+            add(scroll, BorderLayout.CENTER);
+/*
             JScrollPane soaScroll = new JScrollPane(soaTextPane);
             soaScroll.setBorder(null);
             panel.add(soaScroll);
             JScrollPane pScroll = new JScrollPane(pTextPane);
             pScroll.setBorder(null);
             panel.add(pScroll);
+*/
         } else {
+            JPanel panel = new JPanel();
+            panel.setLayout(new GridLayout(rows, cols, hgap, vgap));
             panel.add(soaTextPane);
             panel.add(pTextPane);
+            add(panel, BorderLayout.CENTER);
         }
-        
-        add(panel, BorderLayout.CENTER);
     }
     
     @Override
@@ -73,5 +84,55 @@ public final class KartePanel2 extends KartePanel {
         h += Math.max(hp, hsoa);
 
         return new Dimension(w, h);
+    }
+    
+    // ２号用紙形式KarteEditorのJScrollPaneに入れるパネル
+    // Trick: JTextPaneはScrollableが親ならば幅拡張しない
+    private static final class DoubleScrollPanel extends JPanel implements Scrollable {
+
+        @Override
+        public Dimension getPreferredScrollableViewportSize() {
+            return getParent().getSize();
+        }
+
+        @Override
+        public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+            
+            switch (orientation) {
+                case SwingConstants.VERTICAL:
+                    return visibleRect.height / 10;
+                case SwingConstants.HORIZONTAL:
+                    return visibleRect.width / 10;
+                default:
+                    throw new IllegalArgumentException("Invalid orientation: " + orientation);
+            }
+        }
+
+        @Override
+        public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
+            
+            switch (orientation) {
+                case SwingConstants.VERTICAL:
+                    return visibleRect.height;
+                case SwingConstants.HORIZONTAL:
+                    return visibleRect.width;
+                default:
+                    throw new IllegalArgumentException("Invalid orientation: " + orientation);
+            }
+        }
+
+        @Override
+        public boolean getScrollableTracksViewportWidth() {
+            // DoubleScrollPanelは幅拡張しない
+            return true;
+        }
+
+        @Override
+        public boolean getScrollableTracksViewportHeight() {
+            // これをしないとJTextPaneが小さい場合にウィンドウサイズまで縦に広がらない
+            int portHeight = getParent().getHeight();
+            int height = getPreferredSize().height;
+            return portHeight > height;
+        }
     }
 }
