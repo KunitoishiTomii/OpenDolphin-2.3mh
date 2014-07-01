9e8f6c8a6e9206df9572ceda5c0d5734b9eeab33
Masuda Naika <peu605@yahoo.co.jp>
2014-07-01 23:38:56 +0900

    minor refactoring

--- client/src/main/java/open/dolphin/client/CheckMedication.java
+++ client/src/main/java/open/dolphin/client/CheckMedication.java
@@ -106,42 +106,16 @@ public class CheckMedication {
         for (BundleDolphin bundle : bundleList) {
             String admin = bundle.getAdmin();
             if (admin != null && admin.startsWith("１日")) {
-                //int pos = admin.indexOf("回");
-                //admin = admin.substring("１日".length(), pos);
-                boolean numFound = false;
                 StringBuilder sb = new StringBuilder();
-                int pos = "１日".length();
-                whileLoop:
-                while (pos < admin.length()) {
+                for (int pos = "１日".length(); pos < admin.length(); ++pos) {
                     char c = admin.charAt(pos);
-                    switch (c) {
-                        case '１':
-                        case '２':
-                        case '３':
-                        case '４':
-                        case '５':
-                        case '６':
-                        case '７':
-                        case '８':
-                        case '９':
-                        case '０':
-                            numFound = true;
-                            sb.append(c);
-                            break;
-                        default:
-                            if (numFound) {
-                                break whileLoop;
-                            } else {
-                                break;
-                            }
+                    if ('０' <= c && c <= '９') {
+                        sb.append((char) (c - '０' + '0'));
+                    } else if (sb.length() != 0) {
+                        break;
                     }
-                    pos++;
                 }
-                if (sb.length() == 0) {
-                    admin = null;
-                } else {
-                    admin = ZenkakuUtils.toHalfNumber(sb.toString());
-                }
+                admin = sb.length() == 0 ? null : sb.toString();
             } else {
                 admin = null;
             }
